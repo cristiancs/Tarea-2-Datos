b018123a1e95699d8cd13b2b99a2a17733dceb98
David Medel <dmedel.ba@gmail.com>
2016-06-06 13:40:33 -0300

    archivo comentado y listo(change name list)

--- listas.cpp
+++ listas.cpp
@@ -11,16 +11,20 @@ struct caja{
 	double valor;
 	caja* siguiente;
 };
-
-class Lista_ord{
+// Clase definiendo Lista enlazada
+class Listapoli{
 	caja *principio, *anterior;
 	unsigned int cuantos;
 	bool encontrado;
 	enum donde_enum{VACIO, PRINCIPIO, ENMEDIO, FINAL};
 	donde_enum donde;
+/*
+
+*/
+//Se definen las funciones del TDA .
 public:
-	Lista_ord();
-	~Lista_ord(); 
+	Listapoli();
+	~Listapoli(); 
 	bool buscarValor(unsigned int a, double* coeficiente);
 	bool buscar(double a);
 	bool actualizar(unsigned int a, double valor);
@@ -28,16 +32,29 @@ public:
 	bool borrar();
 	int get_cuantos();
 };
-
-Lista_ord::Lista_ord(){
+/*****
+* Listapoli::Listapoli
+******
+* Defino las casillas NULL , para crear una lista vacia.
+* Returns:
+* no retorna nada
+*****/
+Listapoli::Listapoli(){
 	principio = NULL;
 	anterior = NULL;
 	cuantos = 0;
 	encontrado = false;
 	donde = VACIO;
 }
-
-Lista_ord::~Lista_ord(){
+/*****
+* Listapoli::~Listapoli
+******
+* Libera memoria utilizada en la lista.
+******
+* Returns:
+* no retorna nada
+*****/
+Listapoli::~Listapoli(){
 	caja *p, *q;
 	p = principio;
 	while(p){
@@ -47,9 +64,21 @@ Lista_ord::~Lista_ord(){
 	}
 	cuantos = 0;
 }
+/*****
+* bool Listapoli::actualizar
+******
+* Modifica elemento de la lista en la posición entregada.
+* Reemplaza el valor que ya esta en la lista por el entregado.
+******
+* Input:
+Recibe unsigned int posicion, que indica en que posición se quiere modificar el valor, y también recibe
+double valor , que es el valor que se quiere poner en la posición indicada.
+******
+* Returns:
+* Valor booleano si se realizo bien la actualización.
+*****/
 
-
-bool Lista_ord::actualizar(unsigned int posicion, double valor){
+bool Listapoli::actualizar(unsigned int posicion, double valor){
 	caja *p;
 	unsigned int i = 0;
 	p = principio;
@@ -70,7 +99,19 @@ bool Lista_ord::actualizar(unsigned int posicion, double valor){
 	}
 	return false;
 }
-bool Lista_ord::buscar(double a){
+/*****
+* bool Listapoli::buscar
+******
+* Busca la posición entregada dentro de la lista.
+******
+* Input:
+double a, posición que se busca.
+******
+* Returns:
+* Booleano , para saber si se llega a la posición.
+*****/
+
+bool Listapoli::buscar(double a){
 	caja *p;
 	p = principio;
 	if (!p){
@@ -101,7 +142,23 @@ bool Lista_ord::buscar(double a){
 	donde = FINAL;
 	return false;
 }
-bool Lista_ord::buscarValor(unsigned int posicion, double *coeficiente){
+
+/*****
+* bool Listapoli::buscarValor
+******
+* Obtiene el valor de la posición que se pide , y este se modifica en un puntero,
+asignado para este codigo como "coeficiente".
+******
+* Input:
+unsigned int posicion, la posición de donde se quiere obtener el valor , en este programa la posición corresponde
+al exponente del polinomio.
+double *coeficiente , puntero el cual se modifica para obtener el valor contenido en la posición.
+******
+* Returns:
+* Booleano si se encontro el valor (coeficiente) en la posición(exponente).
+*****/
+
+bool Listapoli::buscarValor(unsigned int posicion, double *coeficiente){
 	caja *p;
 	unsigned int i = 0;
 	p = principio;
@@ -123,7 +180,21 @@ bool Lista_ord::buscarValor(unsigned int posicion, double *coeficiente){
 	}
 	return false;
 }
-bool Lista_ord::push(double a){
+
+/*****
+* bool Listapoli::push
+******
+* Agrega valores a la lista.
+* Principalmente lo utilizamos para iniciar una lista del tamaño del polinomio en cuestión llena de 0.
+******
+* Input:
+*double a ,posición por la cual se irá avanzando para agregar "n" elementos.
+******
+* Returns:
+* True al momento de agregar los valores.
+*****/
+
+bool Listapoli::push(double a){
 	caja *p;
 	buscar(a);
 	p = new caja;
@@ -142,9 +213,18 @@ bool Lista_ord::push(double a){
 	cuantos++;
 	return true;
 }
-
-
-bool Lista_ord::borrar(){
+/*****
+* bool Listapoli::borrar
+******
+* Elimina elemento de la lista
+******
+* Input:
+No recibe nada , solo se le aplica al elemento esta función para eliminarlo de la lista.
+******
+* Returns:
+* Booleano True para confirmar que se realizó correctamente la eliminación.
+*****/
+bool Listapoli::borrar(){
 	caja *p;
 	donde = FINAL;
 	p = anterior -> siguiente;
@@ -154,11 +234,37 @@ bool Lista_ord::borrar(){
 	return true;
 }
 
-int Lista_ord::get_cuantos(){
+/*****
+* int Listapoli::get_cuantos
+******
+* Cuenta cuantos elementos posee la lista.
+* Importante para comprobar que la lista se haya creado correctamente de los n elementos que se le solicitó.
+******
+* Input:
+no recibe nada.
+******
+* Returns:
+* Retorna la cantidad que de elementos contenidos en la lista.
+*****/
+int Listapoli::get_cuantos(){
 	return cuantos;
 }
+/*****
+* double horner
+******
+* Aplica algoritmo de horner al polinomio. Evaluar "x" en el polinomio.
+******
+* Input:
+Listapoli *coeffs, 
+int s, tamaño del polinomio. (exponente mayor)
+double x, valor donde se va a evaluar.
+double *k, puntero a la variable donde se alamacenará el valor buscado
+******
+* Returns:
+* Res, resultado de la aplicación del algoritmo
+*****/
 // Horner
-double horner(Lista_ord *coeffs, int s, double x, double *k)
+double horner(Listapoli *coeffs, int s, double x, double *k)
 {
 	int i;
 	double res = 0.0;
@@ -187,7 +293,8 @@ int main(){
 	//obtener la cantidad y la lectura en que esta
 	getline(polinomiosFile,lectura);
 	cantidad = atoi(lectura.c_str());
-	Lista_ord Poli[cantidad]; 
+	//crear arreglo con la cantidad de listas que se necesita (cantidad de polinomios)
+	Listapoli Poli[cantidad]; 
 
 	// Asigno variables 
 	unsigned int leidos = 0;
@@ -219,7 +326,7 @@ int main(){
 				exponentemayor = exponente;
 			}
 		}	
-
+		// Completar las listas creadas llenandolas de 0, hasta el largo del polinomio , definido por el exponente mayor.
 		unsigned int k=0;
 		while(k<=exponentemayor){
 			Poli[leidos].push(0);
@@ -228,14 +335,17 @@ int main(){
 
 		leidos++;
 	}
+	//Cierro archivo para poder recorrerlo denuevo.
 	polinomiosFile.close();
 
+	//Defino variables similares a las anteriores para una lectura desde el comienzo
 	string lectura1;
 	ifstream polinomiosFile1; 
 	unsigned int cantidad1=0;
 	polinomiosFile1.open("entradaPolinomio.txt"); 
 
 	//obtener la cantidad y la lectura en que esta
+	//se obtiene los valores del exponente, coeficiente, cantidad de polinomios, y monomios que los componen.
 	getline(polinomiosFile1,lectura1);
 	cantidad1 = atoi(lectura1.c_str());
 	leidos=0;
@@ -268,10 +378,11 @@ int main(){
 		}
 		leidos++;
 	}
+	//creo archivo de salida , con variables neesarias.
 	string read;
 	ofstream salidaFile;
 	salidaFile.open("salidaPolinomio.txt");
-	
+	//recorro el archivo de entrada para encontrar valores donde evaluar, y aplicar algoritmo.
 	while (getline(polinomiosFile1,lectura1)){
 		string operaciones[3];
 		unsigned int pos = 0;
@@ -299,8 +410,8 @@ int main(){
 			unsigned int polinomio = atoi(operaciones[1].c_str());
 			unsigned int grado = Poli[polinomio].get_cuantos();
 			double valor = atof(operaciones[2].c_str());
-			// lista de coefientes
 			double *k = (double *) malloc(sizeof(double));
+			//aplico algoritmo de horner
 			double retorno = horner(&Poli[polinomio], grado,valor, k);
 			free((void *) k);
 			salidaFile << retorno << "\n";
@@ -308,13 +419,14 @@ int main(){
 			
 		}
 	}
+	//cierro archivos
 	polinomiosFile1.close();
 	salidaFile.close();
 	i=0;
+	//while para eliminar los bloques generados.
 	while(i<cantidad){
-		Poli[i].~Lista_ord();
+		Poli[i].~Listapoli();
 		i++;
 	}
 	return 0;	
-}			
-
+}	
